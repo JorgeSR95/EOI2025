a9e5dc2b1d166b6d9f5b74e8be851ec6d6f4ee8c
JorgeSR95 <jorgesanchezordenador@gmail.com>
2025-04-22 11:14:15 +0200

    Commit código optimizado

---
+++ src/Library/MostrarMenus.java
@@ -0,0 +1,27 @@
+package Library;
+
+public class MostrarMenus {
+    public static void mostrarMenuPrincipal() {
+        System.out.println("### LIBRERÍA JAVA ###");
+        System.out.println("1. Comprar libro");
+        System.out.println("2. Mostrar información del libro");
+        System.out.println("0. Salir del programa");
+        System.out.println("Elige una opción: ");
+    }
+
+    public static void mostrarMenuCompra() {
+        System.out.println("### COMPRAR LIBRO ###");
+        System.out.println("1. La asistenta");
+        System.out.println("2. Tengo un volcán");
+        System.out.println("0. Salir del programa");
+        System.out.println("Elige una opción: ");
+    }
+
+    public static void mostrarMenuInformacion() {
+        System.out.println("### INFORMACIÓN SOBRE EL LIBRO ###");
+        System.out.println("1. La asistenta");
+        System.out.println("2. Tengo un volcán");
+        System.out.println("0. Salir del programa");
+        System.out.println("Elige una opción: ");
+    }
+}
--- src/Library/VenderLibro.java
+++ src/Library/VenderLibro.java
@@ -16,45 +16,20 @@ public class VenderLibro {
         vendedor1 = new Vendedor("Manolo", 0003436);
         vendedor2 = new Vendedor("Alicia", 0003437);
 
-        mostrarMenuPrincipal();
-        elegirOpcion();
+        MostrarMenus.mostrarMenuPrincipal();
+        elegirOpcionPrincipal();
     }
 
-
-        public static void mostrarMenuPrincipal() {
-            System.out.println("### LIBRERÍA JAVA ###");
-            System.out.println("1. Comprar libro");
-            System.out.println("2. Mostrar información del libro");
-            System.out.println("0. Salir del programa");
-            System.out.println("Elige una opción: ");
-        }
-
-        public static void mostrarMenuCompra() {
-            System.out.println("### COMPRAR LIBRO ###");
-            System.out.println("1. La asistenta");
-            System.out.println("2. Tengo un volcán");
-            System.out.println("0. Salir del programa");
-            System.out.println("Elige una opción: ");
-        }
-
-        public static void mostrarMenuInformacion() {
-            System.out.println("### INFORMACIÓN SOBRE EL LIBRO ###");
-            System.out.println("1. La asistenta");
-            System.out.println("2. Tengo un volcán");
-            System.out.println("0. Salir del programa");
-            System.out.println("Elige una opción: ");
-        }
-
-        public static void elegirOpcion() {
+        public static void elegirOpcionPrincipal() {
             int opcion = LeerJava.getInt();
 
             switch (opcion) {
                 case 1:
-                    mostrarMenuCompra();
+                    MostrarMenus.mostrarMenuCompra();
                     elegirOpcionComprar();
                     break;
                 case 2:
-                    mostrarMenuInformacion();
+                    MostrarMenus.mostrarMenuInformacion();
                     elegirOpcionInformacion();
                     break;
 
